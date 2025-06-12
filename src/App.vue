46a3cefb94b83ecc62290cd44d47a316ddd90c03
Facundo De Lucia <facundodeluciajudo@gmail.com>
2025-06-12 04:31:05 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Mini Cart</h1>
+  <product-list></product-list>
+  <the-cart></the-cart>
+</template>
+
+<script setup>
+import ProductList from './components/ProductList.vue'
+import TheCart from './components/TheCart.vue'
+
+</script>
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ props.product.name }} - ${{ props.product.price }}
+        <button @click="add()">Add to cart</button>
+    </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps(['product'])
+
+function add() {
+
+}
+</script>
